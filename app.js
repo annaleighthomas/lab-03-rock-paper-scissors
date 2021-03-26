9fdba88ea9e645fea8a68171b55c04f151c3e512
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-03-25 21:21:24 -0400

    working click handler

--- app.js
+++ app.js
@@ -1,5 +1,48 @@
-// import functions and grab DOM elements
+import { didUserWin } from './utils.js';
+import { getRandomThrow } from './get-random-throw.js';
 
-// initialize state
 
-// set event listeners to update state and DOM
+const buttonEl = document.querySelector('#button');
+const resetButton = document.querySelector('#reset-button');
+const gameResults = document.querySelector('#game-results');
+const drawCount = document.querySelector('#draw-count');
+const winCount = document.querySelector('#win-count');
+const loseCount = document.querySelector('#lose-count');
+
+
+let draws = 0;
+let wins = 0;
+let losses = 0;
+
+
+buttonEl.addEventListener('click', () => {
+
+
+    const currentlySelectedRadioButton = document.querySelector('input:checked');
+    
+    let computerThrow = getRandomThrow();
+    let playerThrow = currentlySelectedRadioButton.value;
+    let resultOfGame = didUserWin(playerThrow, computerThrow);
+
+    if (resultOfGame === 'draw') {
+        draws++;
+        drawCount.textContent = draws;
+        gameResults.textContent = `It's a Draw!`;
+    } else if (resultOfGame === 'win') {
+        wins++;
+        winCount.textContent = wins;
+        gameResults.textContent = 'You win!';
+    } else {
+        losses++;
+        loseCount.textContent = losses;
+        gameResults.textContent = 'You lose!';
+    }
+});
+
+
+resetButton.addEventListener('click', () => {
+
+    
+
+});
+
